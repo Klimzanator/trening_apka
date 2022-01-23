c85e1d9a527bffb2d8fca325f62cb29ce04a1d12
Klimzanator <klima.mateu@gmail.com>
2022-01-23 12:54:23 +0100

    Changing the key and hiding him

--- training/urls.py
+++ training/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("training/", views.TrainingList.as_view()),
+    path("training/", views.TrainingList.as_view()),     
     path("training/<int:pk>", views.TrainingDetail.as_view()),
     path("training/statistics/", views.statistics),
     
--- training_app/settings.py
+++ training_app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ylg&6mi2t=88hu(*2hp=l@)@e%q!=q$yaer(yy$!53i)7bmim)'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+
+SECRET_KEY = config("SECRET_KEY")
 
 
 # Application definition
